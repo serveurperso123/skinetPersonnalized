40eb8fcf87ead2490c6107d69ca8d8018007342d
jeanne myris <ebogomyris@yahoo.com>
2024-01-31 07:56:28 +0100

    initial commit

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,51 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notfound")]
+        public IActionResult GetNotFoundRequest()
+        {
+            var thing = _context.Products.Find(52);
+
+            if (thing == null)
+            {
+                return NotFound();
+            }
+            return Ok(thing);
+        }
+
+        [HttpGet("servererror")]
+        public IActionResult GetServerError()
+        {
+            var thing = _context.Products.Find(52);
+
+            var thingToReturn = thing.ToString();
+
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public IActionResult GetBadRequest()
+        {
+           
+            return BadRequest();
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public IActionResult GetBadRequest(int id)
+        {
+            
+            return Ok();
+        }
+    }
+}
---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message)
+        {
+            StatusCode = statusCode;
+            Message = message;
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+}
